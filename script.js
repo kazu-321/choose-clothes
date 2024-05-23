c2d7b04fdded6b8dc4d281a6a36be55cdfc3d10b
kazu <kzs321kzs@gmail.com>
2024-05-23 10:01:51 +0900

    update script.js

--- script.js
+++ script.js
@@ -2,18 +2,34 @@ document.getElementById('randomButton').addEventListener('click', () => {
     const topsDir = './clothes/tops/';
     const pantsDir = './clothes/pants/';
     
-    const tops = [
-        'top1.jpg',
-        'top2.jpg',
-        // 他のトップス画像をここに追加
-    ];
+    tops = [];
     
-    const pants = [
-        'pant1.jpg',
-        'pant2.jpg',
-        // 他のパンツ画像をここに追加
-    ];
-    
+    pants = [];
+    // window.webkitRequestFileSystem(
+    //     TEMPORARY,  // 一時的（テンポラリ）
+    //     0,    // 確保するサイズ
+    //     function(fs){ // 成功時のコールバック関数
+    //         fs.root.getDirectory(dirname, {},
+    //             function(dirEntry){
+    //                 var dirReader = dirEntry.createReader();
+    //                 dirReader.readEntries(
+    //                     function(list){
+    //                         // ファイル／ディレクトリの数だけ繰り返し
+    //                         for(var i=0; i<list.length; i++){
+    //                             if (list[i].isFile){  // ファイルの場合
+    //                                 list[i].name
+    //                             }
+    //                         }
+    //                     },
+    //                     function(err){}
+    //                 );
+    //             },
+    //             function(err){}
+    //         );
+    //     },
+    //     function(err){}
+    // )
+
     function getRandomItem(arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
