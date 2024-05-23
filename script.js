b1ac9fe05b47ef2089d178b5d20e1ee1ab46c256
kazu <kzs321kzs@gmail.com>
2024-05-23 12:06:10 +0900

    add functions

--- script.js
+++ script.js
@@ -1,52 +1,28 @@
-document.getElementById('randomButton').addEventListener('click', () => {
-    const topsDir = './clothes/tops/';
-    const pantsDir = './clothes/pants/';
-    window.webkitRequestFileSystem(
-        TEMPORARY,  // 一時的（テンポラリ）
-        0,    // 確保するサイズ
-        function(fs){ // 成功時のコールバック関数
-            fs.root.getDirectory(topsDir, {},
-                function(dirEntry){
-                    var dirReader = dirEntry.createReader();
-                    dirReader.readEntries(
-                        function(list){
-                            // ファイル／ディレクトリの数だけ繰り返し
-                            for(var i=0; i<list.length; i++){
-                                if (list[i].isFile){  // ファイルの場合
-                                    console.log(list[i].name);
-                                }
-                            }
-                        },
-                        function(err){}
-                    );
-                },
-                function(err){}
-            );
-        },
-        function(err){}
-    )
+const owner = 'kazu-321';
+const repo = 'choose-clothes';
+const topsPath = './clothes/tops';
+const pantsPath = './clothes/pants';
 
-    tops = [
-        "tops1.jpeg",
-        "tops2.jpeg",
-        "tops3.jpeg",
-    ];
-    
-    pants = [
-        "pants1.jpeg",
-        "pants2.jpeg",
-        "pants3.jpeg",
-    ];
+async function fetchFilesFromGitHub(path) {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`);
+    const files = await response.json();
+    return files.filter(file => file.type === 'file').map(file => file.download_url);
+}
 
+async function getRandomClothes() {
+    const tops = await fetchFilesFromGitHub(topsPath);
+    const pants = await fetchFilesFromGitHub(pantsPath);
 
     function getRandomItem(arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
     }
-    
+
     const randomTop = getRandomItem(tops);
     const randomPants = getRandomItem(pants);
-    
-    document.getElementById('topImage').src = topsDir + randomTop;
-    document.getElementById('pantsImage').src = pantsDir + randomPants;
-});
+
+    document.getElementById('topImage').src = randomTop;
+    document.getElementById('pantsImage').src = randomPants;
+}
+
+document.getElementById('randomButton').addEventListener('click', getRandomClothes);
