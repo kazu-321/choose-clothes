1fbe1ed9463a1059d80f1eba00b328c9ea90e838
kazu <kzs321kzs@gmail.com>
2024-05-28 08:57:50 +0900

    chore: Refactor script.js for better code organization and initialization process

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const owner = 'kazu-321';
 const repo = 'choose-clothes';
 const topsPath = './clothes/tops';
 const pantsPath = './clothes/pants';
+let pants = [];
+let tops = [];
 
 async function fetchFilesFromGitHub(path) {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`);
@@ -9,20 +11,21 @@ async function fetchFilesFromGitHub(path) {
     return files.filter(file => file.type === 'file').map(file => file.download_url);
 }
 
-async function getRandomClothes() {
-    const tops = await fetchFilesFromGitHub(topsPath);
-    const pants = await fetchFilesFromGitHub(pantsPath);
-
+function getRandomClothes() {    
     function getRandomItem(arr) {
         const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
     }
-
     const randomTop = getRandomItem(tops);
     const randomPants = getRandomItem(pants);
-
     document.getElementById('topImage').src = randomTop;
     document.getElementById('pantsImage').src = randomPants;
 }
 
+async function init() {
+    tops = await fetchFilesFromGitHub(topsPath);
+    pants = await fetchFilesFromGitHub(pantsPath);
+}
+
 document.getElementById('randomButton').addEventListener('click', getRandomClothes);
+document.addEventListener('DOMContentLoaded', init);
